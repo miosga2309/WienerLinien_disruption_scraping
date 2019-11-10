478d1f19f9cec3d1bfff00dcc236a68133f476fe
Jonas Miosga <jonas.miosga@uni-konstanz.de>
2019-11-10 21:13:47 +0100

    soup is ready

--- scraper.py
+++ scraper.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import mysql.connector
+import urllib.request
+from bs4 import BeautifulSoup
+html = urllib.request.urlopen('https://www.wienerlinien.at/eportal3/ep/channelView.do/pageTypeId/66526/channelId/-50038')
+
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup.prettify())

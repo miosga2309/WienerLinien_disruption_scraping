97e5c80f4b7f1f8d4f76dae63d3273eb53bdd7cc
Jonas Miosga <jonas.miosga@uni-konstanz.de>
2019-11-11 14:07:53 +0100

    change of topic

--- scraper.py
+++ scraper.py
@@ -1,14 +1,13 @@
-import mysql.connector
-import urllib.request
+#import mysql.connector
+import requests
 from bs4 import BeautifulSoup
 import re
-html = urllib.request.urlopen('https://www.wienerlinien.at/eportal3/ep/channelView.do/pageTypeId/66526/channelId/-50038')
 
-soup = BeautifulSoup(html, "html.parser")
-
-info = soup.find_all('h2', {"class":"heading-type03"})
-print(info)
-
-#<h2 class="heading-type03">87A : Verkehrsunfall</h2>
-
-#<div class="editor-output ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active" id="ui-accordion-2-panel-0" aria-labelledby="ui-accordion-2-header-0" role="tabpanel" aria-expanded="true" aria-hidden="false" style="display: block;">Nach einer Fahrtbehinderung kommt es auf der Linie 60 zu unterschiedlichen Intervallen.</div>
+def get_menu():
+    r = requests.get('https://www.seezeit.com/essen/speiseplaene/mensa-giessberg/')
+    soup = BeautifulSoup(r.text, features="lxml")
+    food = soup.select("div.contents_aktiv div.speiseplanTagKat")
+    for i in food:
+        cat = i.select(".category")
+        tit = i.select(".title")
+        pri = i.select(".preise")
